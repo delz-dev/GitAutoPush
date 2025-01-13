f4e8ab5f53702e955cf4e98a291878de8f7fd47a
Patrick Delz <patrick@diffrent.ch>
2025-01-13 10:41:56 +0100

    readme update

--- routes/cp.php
+++ routes/cp.php
@@ -3,5 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Diffrentdigital\GitAutoPush\Http\Controllers\SettingsController;
 
-Route::get('git-auto-push/settings', [SettingsController::class, 'index'])->name('git-auto-push.settings');
-Route::post('git-auto-push/settings', [SettingsController::class, 'update'])->name('git-auto-push.settings.update');
+Route::middleware('web')->group(function () {
+    Route::get('git-auto-push/settings', [SettingsController::class, 'index'])->name('git-auto-push.settings');
+    Route::post('git-auto-push/settings', [SettingsController::class, 'update'])->name('git-auto-push.settings.update');
+});
